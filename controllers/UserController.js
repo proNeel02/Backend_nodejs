60c08b0fed1bbe11308541a0d7de8fb319683c81
proNeel02 <prashantdabe02@gmail.com>
2023-06-29 22:45:17 +0530

    adding auth

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,13 @@
 const login = (req, res) => {
   const body = req.body;
-  let err, user;
+
+  const {name, email, phoneNumber} = body;
+
+  if(!name || !email || !phoneNumber){
+    res.status(400).json({message:"Bad Request!"});
+     return;  
+} 
+
   res.status(200).send("User is LoggedIn!");
 };
 module.exports.login = login;
@@ -8,6 +15,13 @@ module.exports.login = login;
 
 const createUser = (req, res) => {
   const body = req.body;
+  
+  const {name, email, phoneNumber} = body;
+
+  if(!name || !email || !phoneNumber){
+    res.status(400).json({message:"Bad Request!"});
+     return;  
+} 
   res.status(201).json({...body,id:Math.floor(Math.random()*10000)});
 };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const UserController = require('../controllers/UserController');
 
 router.post('/login',UserController.login);
