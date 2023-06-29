f4ac1ee33387c2e997c72c7150a27d096c0c1041
proNeel02 <prashantdabe02@gmail.com>
2023-06-29 21:10:48 +0530

    adding

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const app = express();
 // this middle ware will parse body into json object so we can handle clent request more effectively
 app.use(bodyParser.json());
 
-// Below API for testing purpose only
+// Below APIs for testing purpose only
 app.get('/Allcontacts', (req,res) => {
    res.status(200).send("get App is Working Fine!");
 });
+
+app.post('/createContact', (req,res) => {
+    const { name ,email, phone } = req.body;
+    res.status(201).json({name, email, phone});
+ });
+
 
 // listing to port
 app.listen(PORT,() => {
