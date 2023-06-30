00464c969fbed9f6743157c567aa6677d25b3402
proNeel02 <prashantdabe02@gmail.com>
2023-06-30 12:19:46 +0530

    .env

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require("express");
+
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const PORT = 3000;
